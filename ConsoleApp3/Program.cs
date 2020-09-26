181d105ba4064bcfe762bed93a41b983b44e3135
gursk <gursk@DESKTOP-DRD4IPG>
2020-09-26 12:15:23 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USRS_2_Gurskii
+{
+    class Program
+    {
+        public static double a, b;
+        static void Main(string[] args)
+        {
+            Do();
+            {
+                if (a > b && a > 10)
+                {
+                    Console.WriteLine("True"); //sorry , что опоздал, я сделал давно. отправлял по почте, так как на гит не получалось залить, были проблемы с этим
+                }
+                else
+                {
+                    Console.WriteLine("False");
+                }
+                if (a < b || a < 10)
+                {
+                    Console.WriteLine("True");
+                }
+                else
+                {
+                    Console.WriteLine("False");
+                }
+            }
+        }
+        static void Do()
+        {
+            Console.WriteLine("введите первое число");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("введите второе число");
+            b = double.Parse(Console.ReadLine());
+        }
+    }
+}
